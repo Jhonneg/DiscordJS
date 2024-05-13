71d1101da914318b73443c0ae2efc06d0390e0aa
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-13 00:08:47 -0300

    fixed import issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import clientReadyHandler from "./events/ready.js";
 import interactionCreateHandler from "./events/interactionCreate.js";
 import pingCommand from "./commands/utility/ping.js";
+import forecastCommand from "./commands/utility/forecast.js";
 import "dotenv/config";
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -9,6 +10,7 @@ const client = new Client({
 const port = process.env.PORT || 3000;
 client.commands = new Collection();
 
+client.commands.set(forecastCommand.data.name, forecastCommand);
 client.commands.set(pingCommand.data.name, pingCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
